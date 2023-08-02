7196b1b2b4e16f531b26b0697341577ade21a82a
travipati <travipati@deloitte.com>
2023-08-02 12:50:31 -0500

    Added header filter

---
+++ src/main/java/com/deloitte/elrr/datasync/HeaderFilter.java
@@ -0,0 +1,24 @@
+package com.deloitte.elrr.datasync;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class HeaderFilter implements Filter {
+    @Override
+    public
+    void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        if("https".equalsIgnoreCase(httpServletRequest.getHeader("X-Forwarded-Proto"))) {
+            chain.doFilter(request, response);
+        }else {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Not a HTTPS request.");
+        }
+    }
+}
