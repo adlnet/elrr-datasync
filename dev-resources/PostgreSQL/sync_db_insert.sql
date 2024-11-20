88ddc2458239e6e40db5a816301b48d87f6318d6
Leven <phleven@deloitte.com>
2024-11-20 17:14:00 -0500

    Re-factor ,sql.

--- dev-resources/PostgreSQL/sync_db_insert.sql
+++ dev-resources/PostgreSQL/sync_db_insert.sql
@@ -1,7 +1,6 @@
 SET search_path TO staging;
 
 
--- Turncate tables
 TRUNCATE staging.importdetail cascade;
 TRUNCATE staging."import" cascade;
 TRUNCATE staging.syncrecorddetail cascade;
@@ -10,38 +9,45 @@ COMMIT;
 
 
 
--- Restart sequences
-ALTER SEQUENCE staging.import_importid_seq RESTART;
-ALTER SEQUENCE staging.importdetail_importdetailid_seq RESTART;
-ALTER SEQUENCE staging.syncrecord_syncrecordid_seq RESTART;
-ALTER SEQUENCE staging.syncrecorddetail_syncrecorddetailid_seq RESTART;
+-- RESET sequences
+ALTER SEQUENCE staging.import_seq RESTART;
+ALTER SEQUENCE staging.importdetail_seq RESTART;
+ALTER SEQUENCE staging.syncrecord_seq RESTART;
+ALTER SEQUENCE staging.syncrecorddetail_seq RESTART;
 COMMIT;
 
 
 
--- Insert data
-INSERT INTO staging."import" (recordstatus , importname, importstartdate, importenddate)
-VALUES ('SUCCESS', 'Deloitte LRS', '2000-12-30 13:08:54.193', '2000-12-30 13:08:54.193');
+INSERT INTO staging."import" (importid , recordstatus , importname, importstartdate, importenddate)
+VALUES (1, 'SUCCESS', 'Deloitte LRS', '2000-12-30 13:08:54.193', '2000-12-30 13:08:54.193');
 COMMIT;
 
+SELECT setval('staging.import_seq', 1, true);
 
 
-INSERT INTO staging.importdetail (recordstatus, importid, importbegintime, importendtime, totalrecords, successrecords, failedrecords)
-VALUES ('SUCCESS', 1, '2000-12-30 13:08:54.193', '2000-12-30 13:08:54.193',1,1,0);
+
+INSERT INTO staging.importdetail  (importdetailid , recordstatus, importid, importbegintime, importendtime, totalrecords, successrecords, failedrecords)
+VALUES (1, 'SUCCESS', 1, '2000-12-30 13:08:54.193', '2000-12-30 13:08:54.193',1,1,0);
 COMMIT;
 
+SELECT setval('staging.importdetail_seq', 1, true);
 
 
-INSERT INTO staging.syncrecord (recordstatus , synckey, importdetailid)
-VALUES ('inserted', 'Deloitte LRS',1 );
+
+INSERT INTO staging.syncrecord  (syncrecordid , recordstatus , synckey, importdetailid)
+VALUES (1, 'inserted', 'Deloitte LRS',1 );
 COMMIT;
 
+SELECT setval('staging.syncrecord_seq', 1, true);
 
 
-INSERT INTO staging.syncrecorddetail (syncrecordid, recordstatus)
-VALUES (1, 'inserted');
+
+INSERT INTO staging.syncrecorddetail  (syncrecorddetailid, syncrecordid, recordstatus)
+VALUES (1, 1, 'inserted');
 COMMIT;
 
-UPDATE staging.syncrecorddetail set learner = '{"contactEmailAddress":"test@deloitte.com","name":"test","courses":[{"courseId":"5","courseName":"coursename","userCourseStatus":"inserted"}]}'
- WHERE syncrecorddetailid  = 1;
+SELECT setval('staging.syncrecorddetail_seq', 1, true);
+
+update staging.syncrecorddetail set learner = '{"contactEmailAddress":"test@deloitte.com","name":"test","courses":[{"courseId":"5","courseName":"coursename","userCourseStatus":"inserted"}]}'
+ where syncrecorddetailid  = 1;
 COMMIT;
