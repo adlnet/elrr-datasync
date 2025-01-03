4ba756aba42bd64f42619d89c33632bc4d9ede14
Leven <phleven@deloitte.com>
2025-01-03 16:45:21 -0500

    Change synchRecordId to syncRecordId.

--- src/main/java/com/deloitte/elrr/datasync/jpa/service/ErrorsService.java
+++ src/main/java/com/deloitte/elrr/datasync/jpa/service/ErrorsService.java
@@ -20,9 +20,9 @@ public class ErrorsService implements CommonSvc<Errors, Long> {
    * @param errorMsg
    * @return Errors
    */
-  public Errors createErrors(final String synchRecordId, final String errorMsg) {
+  public Errors createErrors(final String syncRecordId, final String errorMsg) {
     Errors errors = new Errors();
-    errors.setErrorMsg("synch record id = " + synchRecordId + " ERROR: " + errorMsg);
+    errors.setErrorMsg("synch record id = " + syncRecordId + " ERROR: " + errorMsg);
     errorsRepository.save(errors);
     return errors;
   }
