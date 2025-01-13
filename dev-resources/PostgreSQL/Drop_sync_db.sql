0583f0e55073443acdeb68dbac74934ceeab21d9
Leven <phleven@deloitte.com>
2025-01-13 15:11:19 -0500

    One .sql script.

--- dev-resources/PostgreSQL/Drop_sync_db.sql
+++
@@ -1,9 +0,0 @@
--- Navigate to elrr schema
-SET search_path = staging;
-
--- Drop staging tables
-DROP TABLE IF EXISTS staging.importdetail cascade;
-DROP TABLE IF EXISTS staging."import" cascade;
-DROP TABLE IF EXISTS staging.syncrecorddetail cascade;
-DROP TABLE IF EXISTS staging.syncrecord cascade;
-DROP TABLE IF EXISTS staging.errors cascade;
