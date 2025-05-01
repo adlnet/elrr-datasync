0bb694008d73573f21767f09dd4f253316a22ff6
Leven <phleven@deloitte.com>
2025-05-01 10:29:50 -0400

    Use HttpServletResponse.SC_FORBIDDEN.

--- src/main/java/com/deloitte/elrr/datasync/HeaderFilter.java
+++ src/main/java/com/deloitte/elrr/datasync/HeaderFilter.java
@@ -39,7 +39,7 @@ public class HeaderFilter implements Filter {
 					chain.doFilter(request, response);
 				} else {
 					log.error("Not a HTTPS request.");
-					((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST,
+					((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN,
 							"Not a HTTPS request.");
 				}
 
