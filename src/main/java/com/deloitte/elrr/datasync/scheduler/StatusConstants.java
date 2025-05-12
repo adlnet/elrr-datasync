8c00e8a226e280832acb0cab8aa1565a0be75749
Leven <phleven@deloitte.com>
2025-05-12 13:49:18 -0400

    Remove adlnet jxapi dependency from .pom.  Change LRS Name to Yet Analytics LRS.

--- src/main/java/com/deloitte/elrr/datasync/scheduler/StatusConstants.java
+++ src/main/java/com/deloitte/elrr/datasync/scheduler/StatusConstants.java
@@ -1,7 +1,7 @@
 package com.deloitte.elrr.datasync.scheduler;
 
 public final class StatusConstants {
-    public static final String LRSNAME = "Deloitte LRS";
+    public static final String LRSNAME = "Yet Analytics LRS";
     public static final String SUCCESS = "SUCCESS";
     public static final String INSERTED = "INSERTED";
     public static final String INPROCESS = "INPROCESS";
