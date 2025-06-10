1b93a988a8606e94ea389b9676cae326125e34e1
Leven <phleven@deloitte.com>
2025-06-10 10:58:59 -0400

    Increase JUnit coverage.

--- src/test/java/com/deloitte/elrr/datasync/JSONRequestSizeLimitFilterTest.java
+++ src/test/java/com/deloitte/elrr/datasync/JSONRequestSizeLimitFilterTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -31,8 +32,32 @@ public class JSONRequestSizeLimitFilterTest {
         logCapture.clear();
 
         ReflectionTestUtils.setField(sl, "maxSizeLimit", 2000000L);
+        ReflectionTestUtils.setField(sl, "checkMediaTypeJson", false);
 
         MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        req.addParameter("anything", "goes");
+        http = new WrappedHttp(req, "{Unwise: napping during work}");
+
+        MockHttpServletResponse res = new MockHttpServletResponse();
+        MockFilterChain chain = new MockFilterChain();
+        sl.doFilter(http, res, chain);
+        assertFalse(res.isCommitted());
+        assertThat(logCapture.getLoggingEvents()).hasSize(0);
+
+    }
+
+    @Test
+    void testMimeType(LogCapture logCapture) throws IOException,
+            ServletException {
+
+        logCapture.clear();
+
+        ReflectionTestUtils.setField(sl, "maxSizeLimit", 2000000L);
+        ReflectionTestUtils.setField(sl, "checkMediaTypeJson", true);
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         req.addParameter("anything", "goes");
         http = new WrappedHttp(req, "{Unwise: napping during work}");
 
